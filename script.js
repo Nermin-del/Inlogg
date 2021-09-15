71d06d9d570f85baf0471a99a44d04cd15866e39
Nermin Festa <nermin_festa@hotmail.com>
2021-09-15 08:48:31 +0200

    Fixat felkod, ändrat om i koden

--- script.js
+++ script.js
@@ -1,87 +1,69 @@
-const btn = document.getElementById("btn"); // Här kopplar jag html och js. Knappfunktionen
-const user = document.getElementById("user"); // Inputen för rutan man skriver in
-const form = document.getElementById("form"); // piffar upp rutan för formen.
-const div = document.createElement("div"); // Här skapar jag ett element via DOM manipulation.
-const lsOutput = document.getElementById("lsOutput"); //LocalStorage output.
-const input = document.getElementById("input");
+const loginButton = document.getElementById("btn"); // Här definerar jag konstanter för att förstå vad jag jobbar med. (Pekar ut sökvägen)
+const userInput = document.getElementById("user"); 
+const passwordInput = document.getElementById("input");
 const username = "test";
-const password = "1234";
-var UserName = document.getElementById("user");
-var PassWord = document.getElementById("input");
+const correctPassword = "1234";
+const logOutButton = document.getElementById("logOutBtn");
+const tryAgainButton = document.getElementById("tryAgainBtn");
 
-
-btn.onclick = function () { // i consolen så finns värdena samlade där när man skriver i rutan.
-    const name = document.getElementById("user").value;
-    const passW = document.getElementById("input").value;
-
-    if (name === username && passW === password) {
-        alert("rätt!");
-        let p = document.createElement("p").innerHTML="Rätt!";
+const onStart = () => {
+    if (checkLogin(localStorage.getItem("UserName"), localStorage.getItem("Password"))) { // här kollar vi localStorage om jag är inloggad fortfarande
+        login();
     } else {
-        alert("fel!");
+        logOutButton.style.display = "none";
+        tryAgainButton.style.display = "none";
     }
-
-    //console.log(name);
-    //console.log(value);
 }
 
-// for (let i = 0; i < localStorage.length; i++) {
-//     const name = localStorage.getItem(user);
-//     const value = localStorage.getItem(input);
-//     // lsOutput.innerText += `$name: ${value}<br />`;
-// }
+const checkLogin = (inputUsername, inputPassword) => { // Se om användaren kan logga in
+    return inputUsername === username && inputPassword === correctPassword; // Validerar användarnamn och lösenord
+};
 
-// div.addEventListener("click", function () { // Tar två parametrar, Event vi lyssnar på, efter det skapas en "callback funktion".
-//     console.log("klicka på knappen!"); // Detta gör så vi får en klick på knappen.
-// })
+const login = () => {
+    let rightPassword = document.createElement("p"); // Skapar ett element med p tagg
+    let text = document.createTextNode("Du har loggat in!"); // istället för innerHTML
+    rightPassword.appendChild(text); // Här appendar jag så det ligger documentet
+    document.body.appendChild(rightPassword); // Här med fast från document.body
 
-// btn.addEventListener("click", () => { //Här skapar jag localStorage så att det sparas när man skriver.
-//     const inputValue = input.value;
-//     localStorage.setItem("user", inputValue.value);
-//     printUser();
-// })
+    userInput.style.display = "none"; // här gömmer jag input för anvädarnamn
+    passwordInput.style.display = "none"; // här gömmer jag input för lösenord
+    loginButton.style.display = "none"; // här gömmer jag login knappen
+    tryAgainBtn.style.display = "none"; // Här döljer jag försök igen knappen
 
-// function validate() { // kolla hur du ska få igenom validation.
+    logOutButton.style.display = "block"; // Här visar jag logoutbutton
+}
 
-//     if (username == null || username == "") {
-//         return false;
-//     }
-//     if (password == null || password == "");
-//     return false;
-// }
+const logout = () => {
+    console.log("du loggade ut");
+}
 
-// const checkUser = () => { //Här skapar jag en if statement. 
-//     const user = localStorage.getItem("test");
+loginButton.onclick = () => { //sätter upp en eventlyssnare på click eventet för login knappen "() => = arrow function"
+    const inputUsername = userInput.value; // Kollar in vad det är för värde i userinput fältet
+    const inputPassword = passwordInput.value; // Kollar in vad det är för värde i passwordinput fältet
+    
 
-//     if (PassWord === password && UserName === username) {
-//         localStorage.setItem(user, password);
-//         alert("Rätt!");
-//         printUser(user);
-//         printUser(input);
-//         return true;
-//     } else {
-//         alert("Fel!");
-//         return false;
-//     }
-// };
+    if (checkLogin(inputUsername, inputPassword)) { // om namn och lösen stämmer så aktiveras hela denna statementen.
+        login();
+        localStorage.setItem("UserName", inputUsername); // Sparar i localStorage namnet
+        localStorage.setItem("Password", inputPassword); // Sparar i localStorage lösenet
+        
 
-// const printUser = (name) => { // Här skapar jag så att när man loggar in att man blir välkommnad!
-//     const user = localStorage.getItem("user");
-//     //div.innerText = `${user}`;
-//     //const nameText = document.createElement("p");
-//     //nameText.innerText = `Ditt namn är ${name}`;
+    } else { // Om det blir fel loggin så visas texten "du skrev fel" och knappen "försök igen" visas
+        let wrongPassword = document.createElement("p");
+        let wrongtext = document.createTextNode("Du skrev fel!"); // istället för innerHTML
+        wrongPassword.appendChild(wrongtext); 
+        document.body.appendChild(wrongPassword);
+        userInput.style.display = "none";
+        passwordInput.style.display = "none";
+        btn.style.display = "none";
+        tryAgainBtn.style.display = "block";
+    }
+}
 
+logOutButton.onclick(() => { // tar bort datan ur localstorage när man loggar ut
+    localStorage.removeItem("UserName");
+    localStorage.removeItem("Password");
+    logout();
+})
 
-//     const eraseButton = document.createElement("button"); // Här skapar jag en knapp så man kan radera namnet vid sidan.
-//     div.appendChild(eraseButton);
-//     eraseButton.innerText = "Radera ";
-//     eraseButton.addEventListener("click", reset);
-// };
-
-// const reset = () => { // här skapar jag så att man kan radera saker på locaStorage.
-//     localStorage.removeItem("user");
-//     div.innerText = "";
-// }
-
-
-//checkUser();
+onStart(); // Detta tar reda på vad man vill visa på hemsidan. När man öppnar index.html så injicerar js filen och kollar om man är inloggad eller inte.
